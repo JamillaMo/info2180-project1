291ca831de11ed37f0cce6c2886649ff43d31801
Jamilla Morgan <jamillamorgan02@gmail.com>
2022-10-31 11:07:35 -0500

    Completed JavaScript section

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 /* Add your JavaScript to this file */
+
+window.addEventListener("load", function (e) {
+    let pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    var btn = document.querySelector("button");
+
+    btn.addEventListener('click', function (e) {
+        e.preventDefault()
+        console.log('Form submitted');
+
+        var email = document.querySelector('#email');
+
+        if(email.value.match(pattern))
+        {
+            document.querySelector(".message").innerHTML = "Thank you! Your email address " + email.value + " has been added to our mailing list.";
+            console.log("Thank you! Your email address " + email.value + " has been added to our mailing list.");
+        }
+
+        else {
+            document.querySelector(".message").innerHTML = "Please enter a valid email address."
+            console.log("Please enter a valid email address.");
+        }
+    
+    });
+
+} )
